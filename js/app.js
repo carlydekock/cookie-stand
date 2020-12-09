d00fe56f57360bcd699108c680a07232abc95153
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2020-12-08 20:32:02 -0800

    Cleaned up old code and comments, commented out start at footer that is unfinished

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 // var limaList = document.getElementById('lima');
 var storeTable = document.getElementById('table');
 var tableHeader = document.getElementById('header');
+// var tableFooter = document.getElementById('footer');
 var stores = [];
 
 // create constructor function to create store
@@ -72,6 +73,21 @@ function renderHeader(){
   thElement.textContent = 'Daily Location Total';
   trElement.appendChild(thElement);
 }
+// //This is the start of trying to get the footer on the table to render with totals
+// function renderFooter(){
+//   var trElement = document.createElement('tr');
+//   tableFooter.appendChild(trElement);
+//   var thElement = document.createElement('th');
+//   thElement.textContent = 'Totals';
+//   trElement.appendChild(thElement);
+//   var allTotal = 0;
+//   for (var i = 0; i < stores.length; i++){
+//     thElement = document.createElement('th');
+//     thElement.textContent = stores[i].hourlySales[0];
+//     trElement.appendChild(thElement);
+//   }
+
+// }
 
 new Store('Seattle', 23, 65, 6.3);
 new Store('Tokyo', 3, 24, 1.2);
@@ -87,258 +103,5 @@ function renderAll(){
 
 renderAll();
 renderHeader();
-
-// //1st object - Seattle Store
-// var seattleStore = {
-//   name: 'Seattle',
-//   min: 23,
-//   max: 65,
-//   avg: 6.3,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-//   //get random number between min and max for number of customers
-//   getRandomNumber: function () {
-//     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-//   },
-
-//   //calculate hourlysales total and populate hourly sales aray
-//   calculateHourlySales() {
-//     for (var i = 0; i < hours.length; i++) {
-//       // console.log(this.getRandomNumber());
-//       // var calcSales = this.getRandomNumber() * this.avg;
-//       var hourlyTotal = Math.ceil(this.getRandomNumber() * this.avg);
-//       // console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   //render list with total at the end
-//   render: function () {
-//     this.calculateHourlySales();
-//     for (var i = 0; i < this.hourlySales.length; i++) {
-//       //Proof I can get here!
-//       // console.log('inside render method');
-//       //create element
-//       var liElement = document.createElement('li');
-//       //give it content
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       //append it to the DOM
-//       seattleList.appendChild(liElement);
-//     }
-//     // render daily total
-//     liElement = document.createElement('li');
-//     // give it content
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     // append it to the DOM
-//     seattleList.appendChild(liElement);
-//   }
-// };
-
-// seattleStore.render();
-// console.log(seattleStore.hourlySales);
-
-// //2nd object - Tokyo Store
-// var tokyoStore = {
-//   name: 'Tokyo',
-//   min: 3,
-//   max: 24,
-//   avg: 1.2,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-//   //get random number between min and max for number of customers
-//   getRandomNumber: function () {
-//     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-//   },
-
-//   //calculate hourlysales total and populate hourly sales aray
-//   calculateHourlySales() {
-//     for (var i = 0; i < hours.length; i++) {
-//       // console.log(this.getRandomNumber());
-//       // var calcSales = this.getRandomNumber() * this.avg;
-//       var hourlyTotal = Math.ceil(this.getRandomNumber() * this.avg);
-//       // console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   //render list with total at the end
-//   render: function () {
-//     this.calculateHourlySales();
-//     for (var i = 0; i < this.hourlySales.length; i++) {
-//       //Proof I can get here!
-//       // console.log('inside render method');
-//       //create element
-//       var liElement = document.createElement('li');
-//       //give it content
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       //append it to the DOM
-//       tokyoList.appendChild(liElement);
-//     }
-//     // render daily total
-//     liElement = document.createElement('li');
-//     // give it content
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     // append it to the DOM
-//     tokyoList.appendChild(liElement);
-//   }
-// };
-
-// tokyoStore.render();
-
-// //3rd object - Dubai
-// var dubaiStore = {
-//   name: 'dubai',
-//   min: 11,
-//   max: 38,
-//   avg: 3.7,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-//   //get random number between min and max for number of customers
-//   getRandomNumber: function () {
-//     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-//   },
-
-//   //calculate hourlysales total and populate hourly sales aray
-//   calculateHourlySales() {
-//     for (var i = 0; i < hours.length; i++) {
-//       // console.log(this.getRandomNumber());
-//       // var calcSales = this.getRandomNumber() * this.avg;
-//       var hourlyTotal = Math.ceil(this.getRandomNumber() * this.avg);
-//       // console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   //render list with total at the end
-//   render: function () {
-//     this.calculateHourlySales();
-//     for (var i = 0; i < this.hourlySales.length; i++) {
-//       //Proof I can get here!
-//       // console.log('inside render method');
-//       //create element
-//       var liElement = document.createElement('li');
-//       //give it content
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       //append it to the DOM
-//       dubaiList.appendChild(liElement);
-//     }
-//     // render daily total
-//     liElement = document.createElement('li');
-//     // give it content
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     // append it to the DOM
-//     dubaiList.appendChild(liElement);
-//   }
-// };
-
-// dubaiStore.render();
-
-
-// //4th object - Paris
-// var parisStore = {
-//   name: 'Paris',
-//   min: 20,
-//   max: 38,
-//   avg: 2.3,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-//   //get random number between min and max for number of customers
-//   getRandomNumber: function () {
-//     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-//   },
-
-//   //calculate hourlysales total and populate hourly sales aray
-//   calculateHourlySales() {
-//     for (var i = 0; i < hours.length; i++) {
-//       // console.log(this.getRandomNumber());
-//       // var calcSales = this.getRandomNumber() * this.avg;
-//       var hourlyTotal = Math.ceil(this.getRandomNumber() * this.avg);
-//       // console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   //render list with total at the end
-//   render: function () {
-//     this.calculateHourlySales();
-//     for (var i = 0; i < this.hourlySales.length; i++) {
-//       //Proof I can get here!
-//       // console.log('inside render method');
-//       //create element
-//       var liElement = document.createElement('li');
-//       //give it content
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       //append it to the DOM
-//       parisList.appendChild(liElement);
-//     }
-//     // render daily total
-//     liElement = document.createElement('li');
-//     // give it content
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     // append it to the DOM
-//     parisList.appendChild(liElement);
-//   }
-// };
-
-// parisStore.render();
-
-
-// //5th object - Lima
-// var limaStore = {
-//   name: 'Lima',
-//   min: 2,
-//   max: 16,
-//   avg: 4.6,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-//   //get random number between min and max for number of customers
-//   getRandomNumber: function () {
-//     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-//   },
-
-//   //calculate hourlysales total and populate hourly sales aray
-//   calculateHourlySales() {
-//     for (var i = 0; i < hours.length; i++) {
-//       // console.log(this.getRandomNumber());
-//       // var calcSales = this.getRandomNumber() * this.avg;
-//       var hourlyTotal = Math.ceil(this.getRandomNumber() * this.avg);
-//       // console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   //render list with total at the end
-//   render: function () {
-//     this.calculateHourlySales();
-//     for (var i = 0; i < this.hourlySales.length; i++) {
-//       //Proof I can get here!
-//       // console.log('inside render method');
-//       //create element
-//       var liElement = document.createElement('li');
-//       //give it content
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       //append it to the DOM
-//       limaList.appendChild(liElement);
-//     }
-//     // render daily total
-//     liElement = document.createElement('li');
-//     // give it content
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     // append it to the DOM
-//     limaList.appendChild(liElement);
-//   }
-// };
-
-// limaStore.render();
 
 
