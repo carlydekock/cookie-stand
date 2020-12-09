6935b44d54febeff45d6905e9115eb4d2d3474b0
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2020-12-09 14:45:13 -0800

    Added function to render footer with totals of sales at bottom of table

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 // var limaList = document.getElementById('lima');
 var storeTable = document.getElementById('table');
 var tableHeader = document.getElementById('header');
-// var tableFooter = document.getElementById('footer');
+var tableFooter = document.getElementById('footer');
 var stores = [];
 
 // create constructor function to create store
@@ -73,21 +73,30 @@ function renderHeader(){
   thElement.textContent = 'Daily Location Total';
   trElement.appendChild(thElement);
 }
-// //This is the start of trying to get the footer on the table to render with totals
-// function renderFooter(){
-//   var trElement = document.createElement('tr');
-//   tableFooter.appendChild(trElement);
-//   var thElement = document.createElement('th');
-//   thElement.textContent = 'Totals';
-//   trElement.appendChild(thElement);
-//   var allTotal = 0;
-//   for (var i = 0; i < stores.length; i++){
-//     thElement = document.createElement('th');
-//     thElement.textContent = stores[i].hourlySales[0];
-//     trElement.appendChild(thElement);
-//   }
-
-// }
+//This is the start of trying to get the footer on the table to render with totals
+function renderFooter(){
+  var trElement = document.createElement('tr');
+  tableFooter.appendChild(trElement);
+  var thElement = document.createElement('th');
+  thElement.textContent = 'Totals';
+  trElement.appendChild(thElement);
+  var allStoresDailyTotal = 0;
+  for (var i = 0; i < hours.length; i++) {
+    var allStoresHourlyTotal = 0;
+    for (var j = 0; j < stores.length; j++) {
+      allStoresHourlyTotal += stores[j].hourlySales[i];
+    }
+    var tdElement = document.createElement('td');
+    tdElement.textContent = allStoresHourlyTotal;
+    trElement.appendChild(tdElement);
+    allStoresDailyTotal += allStoresHourlyTotal;
+    console.log(allStoresDailyTotal);
+  }
+  //Add final grand total in bottom right corner of table
+  tdElement = document.createElement('td');
+  tdElement.textContent = allStoresDailyTotal;
+  trElement.appendChild(tdElement);
+}
 
 new Store('Seattle', 23, 65, 6.3);
 new Store('Tokyo', 3, 24, 1.2);
@@ -103,5 +112,6 @@ function renderAll(){
 
 renderAll();
 renderHeader();
+renderFooter();
 
 
