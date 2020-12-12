64848696ca9987a1d38ccb9c395774062258e70b
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2020-12-11 21:46:10 -0800

    Cleaned up comments, worked on drying out code

--- js/app.js
+++ js/app.js
@@ -3,11 +3,6 @@
 
 // global variables
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-// var seattleList = document.getElementById('seattle');
-// var tokyoList = document.getElementById('tokyo');
-// var dubaiList = document.getElementById('dubai');
-// var parisList = document.getElementById('paris');
-// var limaList = document.getElementById('lima');
 var storeTable = document.getElementById('table');
 var tableHeader = document.getElementById('header');
 var tableFooter = document.getElementById('footer');
@@ -45,96 +40,80 @@ Store.prototype.render = function () {
   var trElement = document.createElement('tr');
   storeTable.appendChild(trElement);
   //create first cell in row, give it name and append to DOM
-  var thElement = document.createElement('th');
-  thElement.textContent = this.name;
-  trElement.appendChild(thElement);
+  renderElement('th', this.name, trElement);
   //iteratively create, give hourly sales content, and append to the DOM
   for (var i = 0; i < this.hourlySales.length; i++) {
-    var tdElement = document.createElement('td');
-    tdElement.textContent = this.hourlySales[i];
-    trElement.appendChild(tdElement);
+    renderElement('td', this.hourlySales[i], trElement);
   }
   // add total to the end of the row
-  tdElement = document.createElement('td');
-  tdElement.textContent = this.dailyTotal;
-  trElement.appendChild(tdElement);
+  renderElement('td', this.dailyTotal, trElement);
 };
+
+//Create function to render new element - to dry out code
+function renderElement (elementCreated, content, parentElement){
+  var childElement = document.createElement(elementCreated);
+  childElement.textContent = content;
+  parentElement.appendChild(childElement);
+}
+
 
 function renderHeader(){
   var trElement = document.createElement('tr');
   tableHeader.appendChild(trElement);
-  var thElement = document.createElement('th');
-  thElement.textContent = 'Stores';
-  trElement.appendChild(thElement);
+  renderElement('th', 'Stores', trElement);
   for (var i = 0; i < hours.length; i++){
-    thElement = document.createElement('th');
-    thElement.textContent = hours[i];
-    trElement.appendChild(thElement);
+    renderElement('th', hours[i], trElement);
   }
   //Add Daily Location Total in cell at end of header
-  thElement = document.createElement('th');
-  thElement.textContent = 'Daily Location Total';
-  trElement.appendChild(thElement);
+  renderElement('th', 'Daily Location Total', trElement);
 }
 // This is the start of trying to get the footer on the table to render with totals
 function renderFooter(){
   var trElement = document.createElement('tr');
   tableFooter.appendChild(trElement);
-  var thElement = document.createElement('th');
-  thElement.textContent = 'Totals';
-  trElement.appendChild(thElement);
+  renderElement('th', 'Totals', trElement);
   var allStoresDailyTotal = 0;
   for (var i = 0; i < hours.length; i++) {
     var allStoresHourlyTotal = 0;
     for (var j = 0; j < stores.length; j++) {
       allStoresHourlyTotal += stores[j].hourlySales[i];
     }
-    var tdElement = document.createElement('td');
-    tdElement.textContent = allStoresHourlyTotal;
-    trElement.appendChild(tdElement);
+    renderElement('td', allStoresHourlyTotal, trElement);
     allStoresDailyTotal += allStoresHourlyTotal;
   }
   //Add final grand total in bottom right corner of table
-  tdElement = document.createElement('td');
-  tdElement.textContent = allStoresDailyTotal;
-  trElement.appendChild(tdElement);
+  renderElement('td', allStoresDailyTotal, trElement);
 }
 
 // Step 3: event handler
 function handleSubmit(event){
   event.preventDefault();
   var storeName = event.target.store.value;
-  console.log(storeName);
-  var customerMin = event.target.min.value;
-  console.log(customerMin);
-  var customerMax = event.target.max.value;
-  console.log(customerMax);
-  var cookieSalesAvg = event.target.avg.value;
-  console.log(cookieSalesAvg);
+  // console.log(storeName);
+  var customerMin = parseInt(event.target.min.value);
+  // console.log(customerMin);
+  var customerMax = parseInt(event.target.max.value);
+  // console.log(customerMax);
+  var cookieSalesAvg = parseInt(event.target.avg.value);
+  // console.log(cookieSalesAvg);
   var newStore = new Store(storeName, customerMin, customerMax, cookieSalesAvg);
   newStore.render();
   //Need to clear current footer, and update with new information
   document.getElementById('footer').innerHTML = '';
   var trElement = document.createElement('tr');
   tableFooter.appendChild(trElement);
-  var thElement = document.createElement('th');
-  thElement.textContent = 'Totals';
-  trElement.appendChild(thElement);
+  renderElement('th', 'Totals', trElement);
   var allStoresDailyTotal = 0;
   for (var i = 0; i < hours.length; i++) {
     var allStoresHourlyTotal = 0;
     for (var j = 0; j < stores.length; j++) {
       allStoresHourlyTotal += stores[j].hourlySales[i];
     }
-    var tdElement = document.createElement('td');
-    tdElement.textContent = allStoresHourlyTotal;
-    trElement.appendChild(tdElement);
+    renderElement('td', allStoresHourlyTotal, trElement);
     allStoresDailyTotal += allStoresHourlyTotal;
   }
   //Add final grand total in bottom right corner of table
-  tdElement = document.createElement('td');
-  tdElement.textContent = allStoresDailyTotal;
-  trElement.appendChild(tdElement);
+  renderElement('td', allStoresDailyTotal, trElement);
 }
 
 // Instantiations of stores
